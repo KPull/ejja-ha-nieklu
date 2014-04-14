1ee523c282d1ef5c450f90c97d4ff0bc105939b3
camilleriluke <mail@camilleriluke.com>
2014-04-15 01:31:13 +0200

    delete order fix

--- webapp/app/scripts/controllers/deleteOrder.js
+++ webapp/app/scripts/controllers/deleteOrder.js
@@ -3,7 +3,7 @@
 angular.module('ikelClientApp').controller('DeleteOrderCtrl', function ($scope, $routeParams, $location, Order) {
     $scope.order = Order.get({ id: $routeParams.orderId });
     $scope.confirm = function() {
-        $scope.order.delete(function() {
+        $scope.order.$delete(function() {
             $location.path('/');
         });
     };
