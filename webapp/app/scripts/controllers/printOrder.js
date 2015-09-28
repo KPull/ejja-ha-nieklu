07a986b481218a8db598b4041a864ff48d694979
Kyle Pullicino <mail@kylepullicino.com>
2015-09-28 20:25:23 +0200

    Added order summary in print order page.

--- webapp/app/scripts/controllers/printOrder.js
+++ webapp/app/scripts/controllers/printOrder.js
@@ -7,6 +7,24 @@ angular.module('ikelClientApp').controller('PrintOrderCtrl', function ($scope, $
       order.total = items.reduce(function(sum, item) {
         return sum + parseFloat(item.price || 0);
       }, 0);
+      
+      // Shrink the order
+      order.summary = items.reduce(function(summary, item) {
+        if (!summary[item.name]) {
+            summary[item.name] = {
+                name: item.name,
+                count: 0,
+                authors: [],
+                subtotal: 0,
+                price: item.price
+            };
+        }
+        summary[item.name].authors.push(item.author);
+        summary[item.name].count = summary[item.name].count + 1;
+        summary[item.name].subtotal = summary[item.name].subtotal + item.price;
+        summary[item.name].price = item.price;
+        return summary;
+      }, { });
     });
   });
 
